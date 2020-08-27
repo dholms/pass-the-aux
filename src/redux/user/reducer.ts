2975d6e8e83020384e3a39136b3924293359e072
Daniel Holmgren <dtholmgren@gmail.com>
2020-08-26 19:39:53 -0500

    check user timeout before loading for localstorage

--- src/redux/user/reducer.ts
+++ src/redux/user/reducer.ts
@@ -12,20 +12,25 @@ export const defaultState = {
   image: null,
 };
 
+// 1 hr
+const SPOTIFY_TIMEOUT = 1000 * 60 * 60
+
 function rehydrate() {
   const user = localStorage.getItem("user");
   if (user) {
-    return JSON.parse(user);
-  } else {
-    return defaultState;
-  }
+    const { token, name, image, time = 0 } = JSON.parse(user);
+    if(Date.now() - time < SPOTIFY_TIMEOUT){
+      return { token, name, image }
+    }
+  } 
+  return defaultState
 }
 
 export default (state = rehydrate(), action: UserAction) => {
   switch (action.type) {
     case USER_LOGGED_IN:
       const { token, name, image } = action.payload;
-      localStorage.setItem("user", JSON.stringify({ token, name, image }));
+      localStorage.setItem("user", JSON.stringify({ token, name, image, time: Date.now() }));
       return {
         ...state,
         token,
